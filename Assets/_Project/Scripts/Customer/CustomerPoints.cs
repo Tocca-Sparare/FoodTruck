a38ee4d58649573956bf7fc4321de775dd79f0fb
redd096 <martin.babino96@gmail.com>
2024-09-11 17:28:57 +0200

    points manager and CustomerPoints on customer prefab

---
+++ Assets/_Project/Scripts/Customer/CustomerPoints.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Give or Remove points when customer is satisfied or unsatisfied
+/// </summary>
+public class CustomerPoints : MonoBehaviour
+{
+    [SerializeField] int pointsToRemoveWhenUnsatisfied;
+    [SerializeField] FPoints[] pointsOnSatisfied;
+
+    Customer customer;
+    PointsManager pointsManager;
+
+    private void Awake()
+    {
+        //get refs
+        pointsManager = FindObjectOfType<PointsManager>();
+        if (customer == null && TryGetComponent(out customer) == false)
+            Debug.LogError($"Missing customer on {name}", gameObject);
+
+        //add events
+        if (customer)
+        {
+            customer.OnSatisfied += OnSatisfied;
+            customer.OnUnsatisfied += OnUnsatisfied;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //remove events
+        if (customer)
+        {
+            customer.OnSatisfied -= OnSatisfied;
+            customer.OnUnsatisfied -= OnUnsatisfied;
+        }
+    }
+
+    private void OnSatisfied()
+    {
+        //find points with this remaining time
+        FPoints addPoints = pointsOnSatisfied.Where(x => customer.RemainingTimeBeforeLeave > x.minimumTime)
+            .OrderBy(x => x.minimumTime).LastOrDefault();       //return bigger one
+
+        //and add
+        if (pointsManager)
+            pointsManager.AddPoints(addPoints.points);
+    }
+
+    private void OnUnsatisfied()
+    {
+        //remove points when leave unsatisfied
+        if (pointsManager)
+            pointsManager.RemovePoints(pointsToRemoveWhenUnsatisfied);
+    }
+
+    [System.Serializable]
+    public struct FPoints
+    {
+        [Tooltip("Give points if remaining time before leave is still bigger than this")]public float minimumTime;
+        public int points;
+    }
+}
---
+++ Assets/_Project/Scripts/Managers/PointsManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>
+/// This is the manager to know how much points the players did in this level
+/// </summary>
+public class PointsManager : MonoBehaviour
+{
+    private int currentPoints;
+    public int CurrentPoints => currentPoints;
+
+    /// <summary>
+    /// Add to current points
+    /// </summary>
+    /// <param name="points"></param>
+    public void AddPoints(int points)
+    {
+        currentPoints += points;
+        Debug.Log($"<color=green>Add {points} = {currentPoints}</color>");
+    }
+
+    /// <summary>
+    /// Remove from current points
+    /// </summary>
+    /// <param name="points"></param>
+    public void RemovePoints(int points)
+    {
+        currentPoints -= points;
+        Debug.Log($"<color=red>Remove {points} = {currentPoints}</color>");
+    }
+}
