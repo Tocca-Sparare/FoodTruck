bfc4a28639b514fa7bcc8f9dd47aa71a0b6f90db
redd096 <martin.babino96@gmail.com>
2024-09-11 13:07:56 +0200

    removed linq in FoodManager

--- Assets/_Project/Scripts/Managers/FoodManager.cs
+++ Assets/_Project/Scripts/Managers/FoodManager.cs
@@ -1,14 +1,27 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
+/// <summary>
+/// Manager in scene with a list of every possible ingredient
+/// </summary>
 public class FoodManager : MonoBehaviour
 {
-    public List<Food> ingredients = new List<Food>();
+    public List<Food> foods = new List<Food>();
 
+    /// <summary>
+    /// Get random food from the list
+    /// </summary>
+    /// <returns></returns>
     public Food GetRandomIngredient()
     {
-        int toSkip = Random.Range(0, ingredients.Count);
-        return ingredients.Skip(toSkip).Take(1).First();
+        if (foods.Count == 0)
+        {
+            Debug.LogError($"There aren't foods in {name}", gameObject);
+            return null;
+        }
+
+        int randomIndex = Random.Range(0, foods.Count);
+        return foods[randomIndex];
+        //return foods.Skip(randomIndex).Take(1).First();
     }
 }
