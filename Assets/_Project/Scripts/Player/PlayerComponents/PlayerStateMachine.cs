cda5da4d2209a914bfc4e458d1349a69fbb9b093
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2024-09-12 13:51:30 +0200

    Revert "add pathCreator and start implementing foodTruckDrive"
    This reverts commit fee0015454cf50e442bbdb16f35d5290a0cff025.

--- Assets/_Project/Scripts/Player/PlayerComponents/PlayerStateMachine.cs
+++ Assets/_Project/Scripts/Player/PlayerComponents/PlayerStateMachine.cs
@@ -6,10 +6,8 @@ public class PlayerStateMachine : BasicStateMachine
 {
     public PlayerNormalState NormalState;
     public PlayerUsingCannonState UsingCannonState;
-    public PlayerDriveState DriveState;
 
     public CannonInteractable Cannon { get; private set; }
-    public FoodTruckDriveInteractable FoodTruckDriver { get; private set; }
 
     /// <summary>
     /// Set cannon to use in PlayerUsingCannonState
@@ -18,14 +16,5 @@ public class PlayerStateMachine : BasicStateMachine
     public void SetCannon(CannonInteractable cannon)
     {
         Cannon = cannon;
-    }
-
-    /// <summary>
-    /// Set FoodTruckDriver to use in PlayerDriveState
-    /// </summary>
-    /// <param name="foodTruckDriver"></param>
-    public void SetFoodTruckDriver(FoodTruckDriveInteractable foodTruckDriver)
-    {
-        FoodTruckDriver = foodTruckDriver;
     }
 }
