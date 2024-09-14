a9bc64162967f65b35003eb5711a4f56aaca41c7
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2024-09-14 15:07:56 +0200

    working truck move on level 3

--- Assets/_Project/Scripts/Obstacles/ObstacleCar.cs
+++ Assets/_Project/Scripts/Obstacles/ObstacleCar.cs
@@ -1,25 +1,42 @@
 using UnityEngine;
 using PathCreation;
 using TMPro;
+using System.Collections.Generic;
+using System.Collections;
 
 public class ObstacleCar : MonoBehaviour
 {
     [SerializeField] float speed = 20;
-    [SerializeField] TMP_Text offsetText;
+    [SerializeField] float stoppingDuration;
+
+    [Header("Offsets")]
     [SerializeField] Vector3 offsetAngle;
+    [SerializeField][Range(0, 1)] float offsetPercentage;
+    [SerializeField] float verticalOffset;
+    
+    [Header("References")]
+    [SerializeField] Transform stoppingPoint;
+    [SerializeField] PathCreator currentPath;
+    [SerializeField] TMP_Text offsetText;
 
-    PathCreator currentPath;
     float distanceTravelled;
-    float offset;
-
-    public bool IsAvailable => currentPath == null;
+    float stoppingDistance;
+    float currentSpeed;
 
     void Start()
     {
         if (currentPath != null)
         {
+            currentSpeed = speed;
+
             // calculate offset
-            distanceTravelled = offset;
+            distanceTravelled = currentPath.path.length * offsetPercentage;
+
+            if (offsetText != null)
+                offsetText.text = offsetPercentage.ToString();
+
+            if (stoppingPoint != null)
+                stoppingDistance = currentPath.path.GetClosestDistanceAlongPath(stoppingPoint.position);
         }
     }
 
@@ -27,21 +44,39 @@ public class ObstacleCar : MonoBehaviour
     {
         if (currentPath != null)
         {
-            distanceTravelled += speed * Time.deltaTime;
-            transform.position = currentPath.path.GetPointAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
-            transform.rotation = currentPath.path.GetRotationAtDistance(distanceTravelled, EndOfPathInstruction.Stop) * Quaternion.Euler(offsetAngle);
+            distanceTravelled += currentSpeed * Time.deltaTime;
+            transform.position = currentPath.path.GetPointAtDistance(distanceTravelled, EndOfPathInstruction.Loop) + verticalOffset * transform.up;
+            transform.rotation = currentPath.path.GetRotationAtDistance(distanceTravelled, EndOfPathInstruction.Loop) * Quaternion.Euler(offsetAngle);
+
+            if (stoppingDistance != -1 && distanceTravelled >= stoppingDistance)
+            {
+                OnStopping();
+            }
 
             if (distanceTravelled >= currentPath.path.length)
             {
-                currentPath = null;
-                gameObject.SetActive(false);
+                OnFinishPath();
             }
         }
     }
 
-    public void SetPath(PathCreator path)
+    void OnStopping()
     {
-        currentPath = path;
+        currentSpeed = 0;
+        stoppingDistance = -1;
+        StartCoroutine(StopCorotuine());
+    }
+
+    void OnFinishPath()
+    {
         distanceTravelled = 0;
+        if (stoppingPoint != null)
+            stoppingDistance = currentPath.path.GetClosestDistanceAlongPath(stoppingPoint.position);
+    }
+
+    IEnumerator StopCorotuine()
+    {
+        yield return new WaitForSeconds(stoppingDuration);
+        currentSpeed = speed;
     }
 }
